a04e17702f797bee03d1ab2d0f328f885fd64391
raizoketh <enridefi221@gmail.com>
2024-07-20 22:21:40 +0200

    Daily update on 2024-07-20 22:21:40.210151

--- index.js
+++ index.js
@@ -65,3 +65,4 @@ const countPrize = element.querySelector(`[class="DrawCard_prizeCount__L+xaL"]`)
   // Added on 2024-07-17 22:01:36.905391
 // Перевірка, чи містить елемент Lucky Draw  // Added on 2024-07-18 20:14:24.586804
 if (cashPrize && countPrize) {  // Added on 2024-07-19 21:53:03.711758
+// Вилучаємо текст з елементів  // Added on 2024-07-20 22:21:40.190227
